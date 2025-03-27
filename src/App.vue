55129abeedc7c7676a5616637f4660171dddd4e1
whutxd <whutxd@163.com>
2025-03-27 13:54:59 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,15 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <!-- <a href="https://vitejs.dev" target="_blank"> -->
+      <!-- <img src="/vite.svg" class="logo" alt="Vite logo" /> -->
+    <!-- </a> -->
+    <!-- <a href="https://vuejs.org/" target="_blank"> -->
+      <!-- <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" /> -->
+    <!-- </a> -->
+    
+    <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    
   </div>
   <HelloWorld msg="Developed By XiaoDong" />
 </template>
