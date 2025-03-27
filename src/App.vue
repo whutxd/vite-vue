baf796164d8163f1e8c649b1c20aa23df4ec8644
whutxd <whutxd@163.com>
2025-03-27 13:58:59 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <!-- <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" /> -->
     <!-- </a> -->
     
-    <img src="./assets/方正logo_透明底色.png" class="logo vue" alt="Vue logo" />
+    <!-- <img src="./assets/方正logo_透明底色.png" class="logo vue" alt="Vue logo" /> -->
+    <img src="./assets/方正logo_透明底色.png" class="logo vue" alt="Vue logo" width="150" height="auto" />
+
     
   </div>
   <HelloWorld msg="Developed By XiaoDong" />
