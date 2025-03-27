75a6e39e5843b61adb12d1ff2ce77e5907eafb7b
whutxd <whutxd@163.com>
2025-03-27 13:32:11 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,19 +16,49 @@ const count = ref(0)
     <!-- 导航链接 -->
     <div class="nav-link">
       <a href="http://fdmai.top/" class="link" target="_blank">
-        <h2>FDM AI平台</h2>
+        <h2>1.FDM AI平台</h2>
+      </a>
+    </div>
+
+    
+    <!-- 导航链接 -->
+    <div class="nav-link">
+      <a href="http://fdmgpt.top" class="link" target="_blank">
+        <h2>2.FDM ChatGPT</h2>
+      </a>
+    </div>
+    
+
+    <!-- 导航链接 -->
+    <div class="nav-link">
+      <a href="http://navi.fdmgpt.top" class="link" target="_blank">
+        <h2>3.FDM ChatGPT（free）</h2>
+      </a>
+    </div>
+
+    
+    <!-- 导航链接 -->
+    <div class="nav-link">
+      <a href="https://navi.fdmgpt.top" class="link" target="_blank">
+        <h2>4.FDM AI效率工具+数字资源库</h2>
+      </a>
+    </div>
+
+    
+    <!-- 导航链接 -->
+    <div class="nav-link">
+      <a href="http://oa.fdm.com.cn/wui/main.jsp?templateId=7" class="link" target="_blank">
+        <h2>5.FDM OA平台</h2>
       </a>
     </div>
 
 
     <!-- 导航链接 -->
     <div class="nav-link">
-      <a href="https://navi.fdmgpt.top" class="link" target="_blank">
-        <h2>友情链接: FDM AI导航站</h2>
+      <a href="https://fdm.com.cn/" class="link" target="_blank">
+        <h2>6.FDM官网</h2>
       </a>
     </div>
-
-
 
 
 
